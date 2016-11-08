0d5dddf721c566e1dda344ce81d32b81c6f2dd04
admin <admin@CHENXIAODAN-PC>
2016-11-08 14:12:17 +0800

    using

--- EFDal/DbContextFactory.cs
+++ EFDal/DbContextFactory.cs
@@ -1,4 +1,9 @@
-﻿using System.Runtime.Remoting.Messaging;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
 using System.Data.Entity;
 using Model;
 
