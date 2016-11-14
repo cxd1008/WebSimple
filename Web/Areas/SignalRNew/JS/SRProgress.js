9ba2f3954004c1e6c66a73b474ecdc39df337db0
admin <admin@CHENXIAODAN-PC>
2016-11-14 10:51:29 +0800

    进度条完成

--- Web/Areas/SignalRNew/JS/SRProgress.js
+++ Web/Areas/SignalRNew/JS/SRProgress.js
@@ -5,7 +5,12 @@ $(function () {
     var bookingHub = $.connection.bookingHub;
     //服务返回
     bookingHub.client.updatecount = function (sCount) {
-        $("#lbCount").text(sCount);
+        $("#lbCount").text(sCount + "%");
+
+        //设置进度条
+        $(".progress-bar").attr({
+            "style": "width:" + sCount + "%"
+        });
     };
     //因为这是长联接，如果用完可以关了就更好了，尝试
     //$.connection.hub.start().done(function () {
