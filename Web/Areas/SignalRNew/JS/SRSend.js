bb93227f0a8b1e294d8e42696995ce53f6fdf61e
admin <admin@CHENXIAODAN-PC>
2016-11-16 11:06:52 +0800

    组聊完成

---
+++ Web/Areas/SignalRNew/JS/SRSend.js
@@ -0,0 +1,46 @@
+﻿/// <reference path="D:\WebMVCSimple\Web\Scripts/jquery-1.10.2.js" />
+/// <reference path="D:\WebMVCSimple\Web\Scripts/jquery.signalR-2.2.1.js" />
+$(function () {
+    //chatHub这个名子很坑，自己写的基本都挂了，开不不明白有大小写之分。后来才知道Hub的开头字母一定要为小写。
+    var chat = $.connection.chatHub;
+    //hello 这个是ChatHub里的一个方法 用$.connection.chatHub读出来的，命名空间随便写。
+    //全体聊天
+    chat.client.helloBack = function (name, message) {
+        $('#discussion').append('<li><strong>' + name + '</strong>对全体说: ' + message + '</li>');
+    };
+    //单聊
+    chat.client.helloSingleBack = function (sid, name, message) {
+        $('#discussion').append('<li><strong>' + name + '</strong>私聊说: ' + message + '</li>');
+    };
+    //组聊
+    chat.client.helloGroupBack = function (sGroupName, name, message) {
+        $('#discussion').append('<li><strong>' + name + '</strong>在' + sGroupName + '组说: ' + message + '</li>');
+    };
+    //返回取得用户ID
+    chat.client.getUserIDBack = function (sid) {
+        $("#sid").val(sid);
+    };
+    //当相于开始主线程
+    $.connection.hub.start().done(function () {
+        chat.server.getUserID("a");//取得当前用户唯一ID
+    });
+    //提交开始一个server程序 群聊
+    $('#sendALL').click(function () {
+        chat.server.hello($('#displayname').val(), $('#message').val());
+        $('#message').val('').focus();
+    });
+    //单聊
+    $('#sendSingle').click(function () {
+        chat.server.helloSingle($("#pid").val(), $('#displayname').val(), $('#message').val());
+        $('#message').val('').focus();
+    });
+    //加入组
+    $("#addgroup").click(function () {
+        chat.server.addGroup($("#sid").val(), $("#groupname").val());
+        alert("成功加入" + $("#groupname").val() + "组");
+    });
+    //组聊
+    $("#sendgroup").click(function () {
+        chat.server.helloGroup($("#groupname").val(), $('#displayname').val(), $('#message').val());
+    });
+});
--- Web/Hub/ChatHub.cs
+++ Web/Hub/ChatHub.cs
@@ -6,22 +6,32 @@ using Microsoft.AspNet.SignalR;
 namespace Web
 {
     public class ChatHub : Hub
-    {       
-        /// 群聊      
+    {
+        /// 群聊
         public void Hello(string name, string message)
         {
             Clients.All.helloBack(name, message);
-        }        
-        /// 单聊       
+        }
+        /// 单聊
         public void HelloSingle(string sid, string name, string message)
         {
-            Clients.Caller.helloSingleBack(sid,name, message);//自己也收到一份消息            
-            Clients.Client(sid).helloSingleBack(sid,name, message);//发送给某人
+            Clients.Caller.helloSingleBack(sid, name, message);//自己也收到一份消息            
+            Clients.Client(sid).helloSingleBack(sid, name, message);//发送给某人
         }
         //得到当前用户ID
         public void GetUserID(string sid)
         {
             Clients.Caller.GetUserIDBack(Context.ConnectionId);
+        }
+        //组聊
+        public void HelloGroup(string sGroupName, string name, string message)
+        {
+            Clients.Group(sGroupName).helloGroupBack(sGroupName, name, message);
+        }
+        //加入一个组
+        public void AddGroup(string sID, string sGroupName)
+        {
+            Groups.Add(sID, sGroupName);
         }
     }
 }
